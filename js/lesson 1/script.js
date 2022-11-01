544dfc067d5813a2d926ac60c50c5e38ccc556f5
Andriy802 <Agavanchak@gmail.com>
2022-11-01 18:08:44 +0200

    Add homework

---
+++ js/lesson 1/script.js
@@ -0,0 +1,46 @@
+let n = 'hello';
+let o = 'owu';
+let p = 'com';
+let d = 'ua';
+let e = 1;
+let f = 10;
+let g = -999;
+let h = 123;
+let i = 3.14;
+let j = 2.7;
+let k = 16;
+let l = true;
+let m = false;
+console.log(n);
+console.log(o);
+console.log(p);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
+console.log(m);
+
+let firstName = "Andriy";
+let middleName = "Igorovuch";
+let lastName = "Gavanchak";
+let person = lastName + " " + firstName + " " + middleName;
+console.log(person);
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+let myFirstName = prompt("Write your First Name");
+let myLastName = prompt("Write your Last Name");
+let age = prompt("Write your age");
+let fullName = myLastName + " " + myFirstName + " " + age;
+console.log(fullName);
