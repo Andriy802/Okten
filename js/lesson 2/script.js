d5b157d80ad5b1d0cd096eac7f30ccd1a054ea3a
Andriy802 <Agavanchak@gmail.com>
2022-11-03 11:29:27 +0200

    Add homework

---
+++ js/lesson 2/script.js
@@ -0,0 +1,123 @@
+let arr = [1, 2, true, "Maria", "love", [5, 6,8, 9], false, 89, "Andriy", 55];
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[8]);
+console.log(arr[9]);
+
+let book1 = {
+    title: "js part1",
+    pageCount: 45,
+    genre: "science"
+};
+let book2 = {
+    title: "js part2",
+    pageCount: 89,
+    genre: "science"
+};
+let book3 = {
+    title: "js part3",
+    pageCount: 123,
+    genre: "science"
+};
+
+let book4 = {
+    title: "js part4",
+    pageCount: 87,
+    genre: "science",
+    authors: [
+        {name: "Anton"},
+        {age: 22}
+    ]
+};
+let book5 = {
+    title: "js part5",
+    pageCount: 203,
+    genre: "science",
+    authors: [
+        {name: "Alex"},
+        {age: 34}
+    ]
+};
+let book6 = {
+    title: "js part6",
+    pageCount: 75,
+    genre: "science",
+    authors: [
+        {name: "Maria"},
+        {age: 19}
+]
+};
+
+let array = [
+    {
+        name: "user1",
+        username: "user111",
+        password: "pass1"
+    },
+    {
+        name: "user2",
+        username: "user222",
+        password: "pass2"
+    },
+    {
+        name: "user3",
+        username: "user333",
+        password: "pass3"
+    },
+    {
+        name: "user4",
+        username: "user444",
+        password: "pass4"
+    },
+    {
+        name: "user5",
+        username: "user555",
+        password: "pass5"
+    },
+    {
+        name: "user6",
+        username: "user666",
+        password: "pass6"
+    },
+    {
+        name: "user7",
+        username: "user777",
+        password: "pass7"
+    },
+    {
+        name: "user8",
+        username: "user888",
+        password: "pass8"
+    },
+    {
+        name: "user9",
+        username: "user999",
+        password: "pass9"
+    },
+    {
+        name: "user10",
+        username: "user101010",
+        password: "pass10"
+    }
+];
+
+console.log(array[0].password);
+console.log(array[1].password);
+console.log(array[2].password);
+console.log(array[3].password);
+console.log(array[4].password);
+console.log(array[5].password);
+console.log(array[6].password);
+console.log(array[7].password);
+console.log(array[8].password);
+console.log(array[9].password);
+
+
+
+
